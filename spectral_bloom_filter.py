73f10648e818781d91de1564dee6a5293ff095a3
iotarepeat <iotarepeat@users.noreply.github.com>
2020-06-12 23:53:18 +0530

    Added gen_counter_chunks
    This method will generate chunks of string.
    E.g. gen_counter_chunks('123456789A', 4, drop_remaining = True) == ['1234', '5678']

--- spectral_bloom_filter.py
+++ spectral_bloom_filter.py
@@ -1,18 +1,41 @@
 import bitarray
-import mmh3 # For murmurhash3
+import mmh3  # For murmurhash3
 import itertools
 from typing import *
 from math import log2
 
 
 class Spectral_Bloom_Filter:
-    def __init__(self,error_rate:float=0.01):
+    def __init__(self, error_rate: float = 0.01):
         self.error_rate = error_rate
 
-    def initialize_string(self, length:int):
-        return ('0'*length)
-    
-    def init_counter(self, counter_length:int) -> dict:
+    def initialize_string(self, length: int):
+        return ('0' * length)
+
+    def gen_counter_chunks(self,
+                           string: str,
+                           chunk_size: int,
+                           drop_remaining: bool = False) -> Iterable[str]:
+        """
+            Yields an iterator of chunks of specified size
+
+            If drop_remaining is specified, the iterator is guaranteed to have  
+            all chunks of same size.
+
+            >>> list(gen_counter_chunks('123456789A', 4)) == ['1234', '5678', '9A']
+            >>> list(gen_counter_chunks('123456789A', 4, drop_remaining = True)) == ['1234', '5678']
+            """
+        string_length = len(string)
+
+        # If drop remaining is True, trim the string
+        if drop_remaining and string_length % chunk_size != 0:
+            closest_multiple = string_length - string_length % chunk_size
+            string = string[:closest_multiple]
+
+        for c in range(0, len(string), chunk_size):
+            yield string[c:c + chunk_size]
+
+    def init_counter(self, counter_length: int) -> dict:
         """
         To initialize a binary counter for incrementing 
         Spectral Bloom Filter's counters.
@@ -23,12 +46,14 @@ class Spectral_Bloom_Filter:
         Params: counter_length - No. of bits in each counter
         
         Returns: Dictionary used for binary counter operation
-        # Look in chat ok!
         """
         size = log2(counter_length)
         digits = list(map(''.join, list(itertools.product('01', repeat=size))))
 
-        bin_counter = {curr: incr for curr,incr in zip(digits, (digits[1:] + [digits[0]]))}
+        bin_counter = {
+            curr: incr
+            for curr, incr in zip(digits, (digits[1:] + [digits[0]]))
+        }
 
         # We don't want last index to be something like '1111' -> '0000',
         # Instead we want something like '1111' -> '1111' (i.e. remain at last position)
