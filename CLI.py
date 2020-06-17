af4c019a982b51347ad5689a47ea888777f4ecac
fork52 <mrunankmistry52@gmail.com>
2020-06-17 12:09:46 +0530

    Improved documentation and renamed.

--- CLI.py
+++ CLI.py
@@ -1,6 +1,8 @@
 import argparse
+from pprint import pprint
 
 def error_rate_arg(val):
+    """Validates the error_rate for the arg parser"""
     try:
         val = float(val)
     except ValueError:
@@ -11,10 +13,11 @@ def error_rate_arg(val):
     raise argparse.ArgumentTypeError(f"{val} not in range [0.0, 1.0]")
 
 def chunk_size_arg(val):
+    """Validates the chunk_size for the arg parser"""
     try:
         val = int(val)
     except ValueError:
-        raise argparse.ArgumentTypeError(f"{val} is not an integer.")
+        raise argparse.ArgumentTypeError(f"{val} is not an integer value.")
 
     #Limit the chunk size to have only 1 to 10 bits.
     if val >= 1 and val <= 10:
@@ -22,21 +25,20 @@ def chunk_size_arg(val):
 
     if val <= 0:
         raise argparse.ArgumentTypeError("Counter size has to be greater than zero.")
-
-    raise argparse.ArgumentTypeError(f"{val} is too high")
+    else:
+        raise argparse.ArgumentTypeError(f"{val} is too high")
 
 def create_arg_parser():
-
-
+    """Returns a well-setup argument-parser object"""
     parser = argparse.ArgumentParser(
         description='Creates a Spectral Bloom filter(SBF) for .html files in the specified directory.'
     )
 
     #The directory argument - posititonal argument
     parser.add_argument(
-        'dir', 
+        'path', 
         type=str, 
-        help='Source directory for creating the filter'
+        help='Path to source directory for creating the filter'
     )
 
     #Error_rate
@@ -46,7 +48,7 @@ def create_arg_parser():
         metavar="ErrorRate",
         dest='error_rate', 
         default= 0.01, 
-        help='Error_rate for the filter  Range:[ 0.0 , 1.0 ]  Default=0.1.'
+        help='Error_rate for the filter  Range:[0.0,1.0]  Default:0.1'
     )
 
     #Counter_size
@@ -56,7 +58,7 @@ def create_arg_parser():
         metavar="Counter_size",
         dest='chunk_size', 
         default= 4, 
-        help='Size in bits of each counter in filter. Default=4(recommended). Range=[1,10]'
+        help='Size in bits of each counter in filter Range:[1,10] Default:4(recommended)'
     )
 
     #Lemmetization
@@ -69,18 +71,18 @@ def create_arg_parser():
 
     #Remove stopwords
     parser.add_argument(
-    '-d',
-        dest='remove_stopwords', 
-        action = 'store_false',
-        help='Disable stopword removal from file (not recommended)'
+    '-ds',
+    dest='remove_stopwords', 
+    action = 'store_false',
+    help='Disable stopword removal from file (not recommended)'
     )
 
     return parser
 
-
 if __name__ == "__main__":
     parser = create_arg_parser()
-    args = vars(parser.parse_args())
 
-    #Arguments which are important
-    print( args )
+    args = vars(parser.parse_args()) #Convert to dictionary
+
+    # args- Arguments which are needed to create the SB filter.
+    pprint( args )
