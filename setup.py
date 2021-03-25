c481287f193a1491a5923c6b5bed39192e0d874f
fork52 <mrunankmistry52@gmail.com>
2021-03-26 00:05:42 +0530

    Updated setup.py. Add requirements.txt.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
           'nltk','bs4','newspaper3k','lxml',
-          'requests'
+          'requests' ,'pytest'
       ],
     classifiers=[
         "Programming Language :: Python :: 3",
